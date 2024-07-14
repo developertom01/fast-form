bb3bfbac7a961b72e9ea53b7ae52af77ab30bd15
Thomas <thomas.sarpong@yum.com>
2024-07-13 21:35:49 -0400

    Implemented pagination on the frontend

--- application/models/pagination.py
+++ application/models/pagination.py
@@ -1,27 +1,33 @@
 from pydantic import BaseModel
 from typing import TypeVar, Generic
-from config import config
+import math
+
 T = TypeVar("T")
 
+
 class PaginationResource(BaseModel, Generic[T]):
-    page:int
-    size:int
-    last:str
-    next: str
+    page: int
+    size: int
+    last: int
+    next: int
     data: list[T]
 
     @staticmethod
-    def get_paginated_object(count:int, size:int, page:str, data: list[T]):
-        app_url = config.get("app_url")
-        last_page = count // size
+    def get_paginated_object(count: int, size: int, page: str, data: list[T]):
+        last = 1
+        if size != 0:
+            last = math.ceil(count / size)
+        
+        next_page = page +1
+        if page == last:
+            next_page = page
 
-        last = f"{app_url}/forms/?page={last_page}"
-        next_page = f"{app_url}/forms/?page={page + 1}"
-
-        return PaginationResource[T](page=page, size=size,last=last, next=next_page,data=data)
+        return PaginationResource[T](
+            page=page, size=size, last=last, next=next_page, data=data
+        )
 
 
 class PaginationParameters(BaseModel):
-    offset:int
-    limit:int
+    offset: int
+    limit: int
     page: int
